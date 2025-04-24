d756d4fd8bc052cd578ebbeea0e8c61239ea34e6
UmutCan <your_email>
2025-04-24 19:34:05 +0300

    solved error

--- js/task-1.js
+++ js/task-1.js
@@ -6,8 +6,9 @@ const isEnoughCapacity = (products, containerSize) => {
     total += obj;
   }
   console.log(total)
-  return total >= containerSize ? false : true;
+  return total <= containerSize ? true : false;
 }
+
 
 console.log(
   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
